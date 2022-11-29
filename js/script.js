c35914d3593a05e347f1acf35aa6fa1c9dc3e2c1
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-11-29 16:57:04 +0100

    odd or even game

--- js/script.js
+++ js/script.js
@@ -1,22 +1,76 @@
+//=====================================================================================================//
+//Task 1 --> PLAINDROME WORDS
+//=====================================================================================================//
 
-//PAROLA PALINDROMA
 
 //define the function
 function isPalindrome(word){
     const array = Array.from(word);
-    let stato = true;
+    let status = true;
     for (let i=0; i<array.length; i++){
         if (  !(array[i] === array[array.length - 1 - i])  ) {
-            stato = false;
+            status = false;
         }
     } 
-    if(stato){
-        console.log('Palindroma!!!');
+    if(status){
+        console.log(word + ' é Palindroma!!!');
     } else {
-        console.log('NON palindroma!!!');
+        console.log(word + ' NON palindroma!!!');
     }
 }
+
 
 //call the function
 const userWord = prompt('Inserisci una parola per verificare se è Palindroma');
 isPalindrome(userWord);
+
+
+//=====================================================================================================//
+//Task 2 --> ODD OR EVEN
+//=====================================================================================================//
+
+
+//define function for random number in a range
+function randomNumberRange(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1) + min);
+    return num;
+}
+
+
+//define function for say if a number is odd or even
+function isSumOddOrEven(num1, num2) {
+    const sum = num1 + num2;
+    if ((sum % 2) == 0){
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
+
+
+//call the bet
+let userOddEven;
+do {
+    userOddEven = prompt('Vuoi puntare su "pari" o "dispari" ?');
+} while ( !((userOddEven == 'pari') || (userOddEven == 'dispari')) );
+console.log('Hai scielto si puntare su: ' + userOddEven);
+
+
+//call the number
+let userNumber;
+do {
+    userNumber = parseInt(prompt('Inserisci un valore compreso tra 1 e 5" ?'));
+} while ( !((userNumber >= 1) && (userNumber <= 5)) );
+console.log('Hai scielto il numero: ' + userNumber);
+
+
+//check the win
+const pcNumber = randomNumberRange(1, 5);
+console.log('Il PC ha scielto il numero: ' + pcNumber)
+const sumStatus = isSumOddOrEven(userNumber, pcNumber);
+
+if ( sumStatus === userOddEven ) {
+    console.log('Hai vinto il gioco dei numeri!!');
+} else {
+    console.log('Hai vinto il gioco dei numeri!!');
+}
